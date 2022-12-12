42b7cf1d5a556fc6e8c6e69d87b2ba87e3fe14b5
Margo <m4ngom@yandex.kz>
2022-12-12 21:16:20 +0300

    test_items.py added

---
+++ test_items.py
@@ -0,0 +1,15 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_the_basket_link(browser):
+    browser.get(link)
+    time.sleep(20)
+    basket_link = browser.find_elements(By.CSS_SELECTOR, "span.btn-group a")
+    assert len(basket_link) == 1 , "Нет кнопки корзины"
+
+
+
