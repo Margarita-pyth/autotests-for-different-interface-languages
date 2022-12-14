a509861eb28025a7bbe9bd65bbc46943588988f9
Margo <m4ngom@yandex.kz>
2022-12-14 10:02:49 +0300

    added selectors for example

--- test_items.py
+++ test_items.py
@@ -8,7 +8,10 @@ link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_the_basket_link(browser):
     browser.get(link)
     time.sleep(20)
-    basket_link = browser.find_elements(By.CSS_SELECTOR, "span.btn-group a")
+    # basket_link = browser.find_elements(By.CSS_SELECTOR, "span.btn-group a")
+    # browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    # assert browser.find_element(By.CSS_SELECTOR, 'span.btn-group > a.btn')
+    basket_link = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
     assert len(basket_link) == 1 , "Нет кнопки корзины"
 
 # pytest --language=es команда запуска скрипта (по умолчанию выполнение в Chrome)
